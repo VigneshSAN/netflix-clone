b0588cfe6d6a042a546490c70843fc6bb7efe42e
Xreon Tech <58514176+XreonTech@users.noreply.github.com>
2022-08-20 18:55:02 +0530

    adding eslint for programming standard

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -12,6 +12,7 @@ export class NavBarComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    console.log("Iniating navbar");
   }
 
   @HostListener('window:scroll', ['$event'])
